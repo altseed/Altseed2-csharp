8e3e96a97c47f6516a0f636ad56795819d6c1508
wisteria <wisteria@coins.tsukuba.ac.jp>
2019-10-25 10:50:22 +0900

    clean up

--- Scripts/GenerateBinding.bat
+++
@@ -1,9 +0,0 @@
-cd %~dp0..\Core\scripts
-FOR /f "usebackq tokens=*" %%i IN (`cd`) DO set PYTHONPATH=%%i
-
-cd %~dp0..\CBG
-FOR /f "usebackq tokens=*" %%i IN (`cd`) DO set PYTHONPATH=%%i;%PYTHONPATH%
-
-python %~dp0generate_binding.py
-
-pause
