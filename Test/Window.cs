8a51484c70e265f3d5a307d6e1f5cc279ec7214d
wisteria <wisteria@coins.tsukuba.ac.jp>
2020-01-26 17:39:30 +0900

    add: Window Test

---
+++ Test/Window.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace Altseed.Test
+{
+    public class Window
+    {
+        [Fact]
+        public void Base()
+        {
+            var coreOption = new CoreOption()
+            {
+                IsFullscreenMode = false,
+                IsResizable = false,
+            };
+
+            Assert.True(Engine.Initialize("Altseed2 C# Engine", 800, 600, coreOption));
+
+            while (Engine.DoEvents())
+            {
+                Engine.Update();
+            }
+
+            Engine.Terminate();
+        }
+    }
+}
