6de4cf3263f4637c9392256becd70182d19656b2
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-02-04 18:18:23 +0900

    implement:StaticFile

--- Engine/InternalHelper/IOHelper.cs
+++ Engine/InternalHelper/IOHelper.cs
@@ -7,6 +7,24 @@ namespace Altseed
     {
         private static char[] InvalidChars => _invalidChars ?? (_invalidChars = Path.GetInvalidPathChars());
         private static char[] _invalidChars;
+        internal static Exception CheckLoadPath(string path)
+        {
+            if (path == null) return new ArgumentNullException("引数がnullです", nameof(path));
+            if (string.IsNullOrWhiteSpace(path)) return new ArgumentException("パスが空白文字のみからなります", nameof(path));
+            if (ContainsInvalidChar(path)) return new ArgumentException("パスに不正な文字が含まれています", nameof(path));
+            if (IsTooLongPath(path)) return new PathTooLongException("パスが長すぎます");
+            if (!Engine.File.Exists(path)) return new FileNotFoundException("指定したパスのファイルが見つかりませんでした", path);
+            return null;
+        } 
+        internal static Exception CheckSavePath(string path)
+        {
+            if (path == null) return new ArgumentNullException("引数がnullです", nameof(path));
+            if (string.IsNullOrWhiteSpace(path)) return new ArgumentException("パスが空白文字のみからなります", nameof(path));
+            if (ContainsInvalidChar(path)) return new ArgumentException("パスに不正な文字が含まれています", nameof(path));
+            if (IsTooLongPath(path)) return new PathTooLongException("パスが長すぎます");
+            if (!Directory.Exists(Path.GetDirectoryName(path))) return new DirectoryNotFoundException($"指定したパスのディレクトリが見つかりませんでした：{Path.GetDirectoryName(path)}");
+            return null;
+        } 
         internal static bool ContainsInvalidChar(string path)
         {
             if (path == null) throw new ArgumentNullException("引数がnullです", nameof(path));
@@ -16,15 +34,6 @@ namespace Altseed
                         return true;
             return false;
         }
-        internal static Exception IsCompatiblePath(string path)
-        {
-            if (path == null) return new ArgumentNullException("引数がnullです", nameof(path));
-            if (string.IsNullOrWhiteSpace(path)) return new ArgumentException("パスが空白文字のみからなります", nameof(path));
-            if (ContainsInvalidChar(path)) return new ArgumentException("パスに不正な文字が含まれています", nameof(path));
-            if (IsTooLongPath(path)) return new PathTooLongException("パスが長すぎます");
-            if (!Engine.File.Exists(path)) return new FileNotFoundException("指定したパスのファイルが見つかりませんでした", path);
-            return null;
-        } 
         internal static bool IsTooLongPath(string path)
         {
             if (path == null) throw new ArgumentNullException("引数がnullです", nameof(path));
