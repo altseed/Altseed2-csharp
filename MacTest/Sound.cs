ffa1f8fa50363b406e4ef12b4ca6b120d001070b
Lemon TEA <green.c.lemon@gmail.com>
2020-02-20 22:41:51 +0900

    Add test project for Mac

---
+++ MacTest/Sound.cs
@@ -0,0 +1,25 @@
+using Altseed;
+
+namespace MacTest
+{
+    class SoundTest
+    {
+        public static void Play()
+        {
+            Engine.Initialize("SoundTest Play", 640, 480);
+
+            Sound bgm = Engine.Sound.Load("./TestData/Sound/bgm1.ogg", false);
+            Sound se = Engine.Sound.Load("./TestData/Sound/se1.wav", true);
+
+            int bgm_id = Engine.Sound.Play(bgm);
+            int se_id = Engine.Sound.Play(se);
+
+            while(Engine.DoEvents() && (Engine.Sound.GetIsPlaying(bgm_id) || Engine.Sound.GetIsPlaying(se_id)))
+            {
+                Engine.Update();
+            }
+
+            Engine.Terminate();
+        }
+    }
+}
