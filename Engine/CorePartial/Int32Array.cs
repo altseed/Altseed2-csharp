2edc955326d1e439c8b86bc78aa50cdb38912937
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-02-22 00:25:19 +0900

    add:ArrayConvert method

---
+++ Engine/CorePartial/Int32Array.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Altseed
+{
+    public partial class Int32Array
+    {
+
+        /// <summary>
+        /// <see cref="int"/>型の配列に変換する
+        /// </summary>
+        /// <returns>このインスタンスのデータと等しい<see cref="int"/>配列のインスタンス</returns>
+        public int[] ToIntArray()
+        {
+            var array = new int[Count];
+            Marshal.Copy(selfPtr, array, 0, Count);
+            return array;
+        }
+
+        /// <summary>
+        /// <see cref="int"/>型の配列から<see cref="Int32Array"/>に型変換する
+        /// </summary>
+        /// <param name="array">データを格納する<see cref="int"/>型の配列</param>
+        /// <exception cref="ArgumentNullException"><paramref name="array"/>がnull</exception>
+        /// <exception cref="SystemException">インスタンスの生成に失敗した</exception>
+        /// <returns><paramref name="array"/>と同じデータを格納する<see cref="Int32Array"/>のインスタンス</returns>
+        public static Int32Array FromIntArray(int[] array)
+        {
+            if (array == null) throw new ArgumentNullException("引数がnullです", nameof(array));
+
+            var result = new Int32Array(new MemoryHandle(IntPtr.Zero));
+
+            Marshal.Copy(array, 0, result.selfPtr, array.Length);
+
+            return result;
+        }
+
+        public static explicit operator int[](Int32Array a) => a.ToIntArray();
+    }
+}
---
+++ Engine/CorePartial/Int8Array.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Altseed
+{
+    public partial class Int8Array
+    {
+        /// <summary>
+        /// <see cref="byte"/>型の配列に変換する
+        /// </summary>
+        /// <returns>このインスタンスのデータと等しい<see cref="byte"/>配列のインスタンス</returns>
+        public byte[] ToByteArray()
+        {
+            var array = new byte[Count];
+            Marshal.Copy(selfPtr, array, 0, Count);
+            return array;
+        }
+
+        /// <summary>
+        /// <see cref="byte"/>型の配列から<see cref="Int8Array"/>に型変換する
+        /// </summary>
+        /// <param name="array">データを格納する<see cref="byte"/>型の配列</param>
+        /// <exception cref="ArgumentNullException"><paramref name="array"/>がnull</exception>
+        /// <exception cref="SystemException">インスタンスの生成に失敗した</exception>
+        /// <returns><paramref name="array"/>と同じデータを格納する<see cref="Int8Array"/>のインスタンス</returns>
+        public static Int8Array FromByteArray(byte[] array)
+        {
+            if (array == null) throw new ArgumentNullException("引数がnullです", nameof(array));
+
+            var result = new Int8Array(new MemoryHandle(IntPtr.Zero));
+
+            Marshal.Copy(array, 0, result.selfPtr, array.Length);
+
+            return result;
+        }
+
+        public static explicit operator byte[](Int8Array a) => a.ToByteArray();
+    }
+}
