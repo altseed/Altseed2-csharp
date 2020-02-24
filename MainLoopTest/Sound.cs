1275a31adea129bf8a62142fdf580041796f0757
Lemon TEA <green.c.lemon@gmail.com>
2020-02-24 17:55:45 +0900

    Add sound looping test

--- MainLoopTest/Sound.cs
+++ MainLoopTest/Sound.cs
@@ -8,13 +8,32 @@ namespace MacTest
         {
             Engine.Initialize("SoundTest Play", 640, 480);
 
-            Sound bgm = Engine.Sound.Load(@"../Core/TestData/Sound/bgm1.ogg", false);
-            Sound se = Engine.Sound.Load(@"../Core/TestData/Sound/se1.wav", true);
+            Sound bgm = Sound.Load(@"../Core/TestData/Sound/bgm1.ogg", false);
+            Sound se = Sound.Load(@"../Core/TestData/Sound/se1.wav", true);
 
             int bgm_id = Engine.Sound.Play(bgm);
             int se_id = Engine.Sound.Play(se);
 
             while(Engine.DoEvents() && (Engine.Sound.GetIsPlaying(bgm_id) || Engine.Sound.GetIsPlaying(se_id)))
+            {
+                Engine.Update();
+            }
+
+            Engine.Terminate();
+        }
+
+        public static void Loop()
+        {
+            Engine.Initialize("SoundTest Loop", 640, 480);
+
+            Sound bgm = Sound.Load(@"../Core/TestData/Sound/bgm1.ogg", false);
+            bgm.IsLoopingMode = true;
+            bgm.LoopStartingPoint = 1.0f;
+            bgm.LoopEndPoint = 2.5f;
+
+            int bgm_id = Engine.Sound.Play(bgm);
+
+            while(Engine.DoEvents() && Engine.Sound.GetIsPlaying(bgm_id) )
             {
                 Engine.Update();
             }
