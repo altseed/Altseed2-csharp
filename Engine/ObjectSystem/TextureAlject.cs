86187c3109e09ed8331d6e7a99c67bc3abc1a7d1
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-02-26 08:49:58 +0900

    add:TextureObject

---
+++ Engine/ObjectSystem/TextureAlject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Altseed
+{
+    /// <summary>
+    /// テクスチャを描画する<see cref="Alject"/>のクラス
+    /// </summary>
+    [Serializable]
+    public class TextureAlject : Alject
+    {
+        private readonly TextureComponent textureComponent;
+
+        /// <summary>
+        /// 回転角度(度数法)を取得または設定する
+        /// </summary>
+        public float Angle { get => textureComponent.Angle; set => textureComponent.Angle = value; }
+
+        /// <summary>
+        /// 座標を取得または設定する
+        /// </summary>
+        public Vector2F Position { get => textureComponent.Position; set => textureComponent.Position = value; }
+
+        /// <summary>
+        /// 拡大率を取得または設定する
+        /// </summary>
+        public Vector2F Scale { get => textureComponent.Scale; set => textureComponent.Scale = value; }
+
+        /// <summary>
+        /// 描画するテクスチャの領域を取得または設定する
+        /// </summary>
+        public RectF Src { get => textureComponent.Src; set => textureComponent.Src = value; }
+
+        /// <summary>
+        /// 描画するテクスチャを取得または設定する
+        /// </summary>
+        public Texture2D Texture { get => textureComponent.Texture; set => textureComponent.Texture = value; }
+
+        /// <summary>
+        /// 新しいインスタンスを生成する
+        /// </summary>
+        public TextureAlject()
+        {
+            textureComponent = new TextureComponent();
+            AddComponent(textureComponent);
+        }
+    }
+}
