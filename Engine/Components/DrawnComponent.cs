89c9dd0ed79fba58224647ee22ccfa09c502ac3f
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-02-28 00:11:41 +0900

    implement:serialization

--- Engine/Components/DrawnComponent.cs
+++ Engine/Components/DrawnComponent.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Altseed
 {
     /// <summary>
     /// 描画コンポーネントを表します。
     /// </summary>
+    [Serializable]
     public abstract class DrawnComponent : AljectComponent
     {
         /// <summary>
