324a7a922484e17229377e425ece8deeb912e36b
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-02-28 17:59:36 +0900

    add:TextObject & Test

---
+++ Engine/Components/TextComponent.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Altseed
+{
+    /// <summary>
+    /// テキストの描画を行うコンポーネント
+    /// </summary>
+    [Serializable]
+    public class TextComponent : DrawnComponent, IDeserializationCallback
+    {
+        private readonly List<RenderedSprite> sprites = new List<RenderedSprite>();
+
+        /// <summary>
+        /// 角度を取得または設定する
+        /// </summary>
+        public override float Angle
+        {
+            get => _angle;
+            set
+            {
+                if (_angle != value)
+                {
+                    _angle = value;
+                    M_angle.SetRotationZ(value);
+                    UpdateImages();
+                }
+            }
+        }
+        private float _angle;
+        private Matrix44F M_angle = Matrix44F.GetIdentity();
+
+        /// <summary>
+        /// 描画するフォントを取得または設定する
+        /// </summary>
+        public Font Font
+        {
+            get => _font;
+            set
+            {
+                if (_font != value)
+                {
+                    _font = value;
+                    UpdateImages();
+                }
+            }
+        }
+        private Font _font;
+
+        public Material Material
+        {
+            get => _material;
+            set
+            {
+                if (_material != value)
+                {
+                    _material = value;
+                    UpdateImages();
+                }
+            }
+        }
+        private Material _material = new Material();
+
+        /// <summary>
+        /// 座標を取得または設定する
+        /// </summary>
+        public override Vector2F Position
+        {
+            get => _position;
+            set
+            {
+                if (value != _position)
+                {
+                    _position = value;
+                    M_position.SetTranslation(value.X, value.Y, 0.0f);
+                    UpdateImages();
+                }
+            }
+        }
+        [NonSerialized]
+        private Vector2F _position;
+        [NonSerialized]
+        private Matrix44F M_position = Matrix44F.GetIdentity();
+
+        /// <summary>
+        /// 拡大率を取得または設定する
+        /// </summary>
+        public override Vector2F Scale
+        {
+            get => _scale;
+            set
+            {
+                if (value != _scale)
+                {
+                    _scale = value;
+                    M_scale.SetScale(value.X, value.Y, 1.0f);
+                    UpdateImages();
+                }
+            }
+        }
+        private Vector2F _scale = new Vector2F(1.0f, 1.0f);
+        [NonSerialized]
+        private Matrix44F M_scale = Matrix44F.GetIdentity();
+
+        /// <summary>
+        /// 描画するテキストを取得または設定する
+        /// </summary>
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value ?? string.Empty;
+                    UpdateImages(true);
+                }
+            }
+        }
+        private string _text = string.Empty;
+
+        /// <summary>
+        /// 新しいインスタンスを生成する
+        /// </summary>
+        public TextComponent()
+        {
+            _material.Shader = Engine.Graphics.BuildinShader.Create(BuildinShaderType.FontUnlitPS);
+            UpdateImages();
+        }
+
+        internal override void Draw()
+        {
+            foreach (var s in sprites) Engine.Renderer.DrawSprite(s);
+        }
+
+        /// <summary>
+        /// デシリアライズ時に実行
+        /// </summary>
+        /// <param name="sender">現段階では実装されていない 常にnullを返す</param>
+        protected virtual void OnDeserialization(object sender)
+        {
+            M_position.SetTranslation(_position.X, _position.Y, 0.0f);
+            M_scale.SetScale(_scale.X, _scale.Y, 1.0f);
+        }
+        void IDeserializationCallback.OnDeserialization(object sender) => OnDeserialization(sender);
+
+        /// <summary>
+        /// 描画する情報を更新する
+        /// </summary>
+        internal void UpdateImages(bool clear = false)
+        {
+            if (clear) sprites.Clear();
+            var position = _position;
+            for (int i = 0; i < _text.Length; i++)
+            {
+                var glyph = Font.GetGlyph(_text[i]);
+                if (glyph == null) continue;
+
+                var tempPosition = position + glyph.Offset + new Vector2F(0, _font.Ascent);
+                var sprite = RenderedSprite.Create();
+                sprite.Material = _material;
+                sprite.Texture = _font.GetFontTexture(glyph.TextureIndex);
+
+                var trans = new Matrix44F();
+                trans.SetTranslation(tempPosition.X, tempPosition.Y, 0);
+                sprite.Transform = trans;
+
+                sprite.Src = new RectF(glyph.Position.X, glyph.Position.Y, glyph.Size.X, glyph.Size.Y);
+
+                if (clear) sprites.Add(sprite);
+                else sprites[i] = sprite;
+
+                position += new Vector2F(glyph.GlyphWidth, 0);
+
+                if (i < _text.Length - 1) position += new Vector2F(_font.GetKerning(_text[i], _text[i + 1]), 0);
+            }
+        }
+    }
+}
---
+++ Engine/ObjectSystem/TextAlject.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Altseed
+{
+    /// <summary>
+    /// テキストを描画する<see cref="DrawnAlject"/>のクラス
+    /// </summary>
+    [Serializable]
+    public class TextAlject : DrawnAlject
+    {
+        private readonly TextComponent textComponent;
+
+        /// <summary>
+        /// 角度を取得または設定する
+        /// </summary>
+        public override float Angle
+        {
+            get => textComponent.Angle; 
+            set { textComponent.Angle = value; }
+        }
+
+        /// <summary>
+        /// 描画するフォントを取得または設定する
+        /// </summary>
+        public Font Font
+        {
+            get => textComponent.Font;
+            set { textComponent.Font = value; }
+        }
+
+        /// <summary>
+        /// 使用するマテリアルを取得または設定する
+        /// </summary>
+        public Material Material
+        {
+            get => textComponent.Material;
+            set { textComponent.Material = value; }
+        }
+
+        /// <summary>
+        /// 座標を取得または設定する
+        /// </summary>
+        public override Vector2F Position
+        {
+            get => textComponent.Position;
+            set { textComponent.Position = value; }
+        }
+
+        /// <summary>
+        /// 拡大率を取得または設定する
+        /// </summary>
+        public override Vector2F Scale
+        {
+            get => textComponent.Scale;
+            set { textComponent.Scale = value; }
+        }
+
+        /// <summary>
+        /// テキストを取得または設定する
+        /// </summary>
+        public string Text
+        {
+            get => textComponent.Text;
+            set { textComponent.Text = value; }
+        }
+
+        /// <summary>
+        /// 新しいインスタンスを生成する
+        /// </summary>
+        public TextAlject()
+        {
+            textComponent = new TextComponent();
+            AddComponent(textComponent);
+        }
+
+
+    }
+}
