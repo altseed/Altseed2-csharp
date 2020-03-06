b6c91403a236f51ac0d26d5e123f36c4e28251e0
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-03-06 18:46:06 +0900

    change:ToStaticMethods_33I

--- Engine/Math/Matrix44I.cs
+++ Engine/Math/Matrix44I.cs
@@ -144,8 +144,7 @@ namespace Altseed
         /// </summary>
         public void SetIdentity()
         {
-            if (Values == null)
-                Values = new int[4, 4];
+            Values ??= new int[4, 4];
 
             for (int i = 0; i < 4; i++)
                 for (int j = 0; j < 4; j++)
