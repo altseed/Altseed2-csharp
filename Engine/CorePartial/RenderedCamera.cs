6cb09e647f2ca0a98a19c3f22d6dc4488b678217
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-03-21 04:34:52 +0900

    implement:camera Serialization

--- Engine/CorePartial/RenderedCamera.cs
+++ Engine/CorePartial/RenderedCamera.cs
@@ -7,6 +7,8 @@ namespace Altseed
     internal partial class RenderedCamera : ISerializable
     {
         #region SerializeName
+        private const string S_CenterOffSet = "S_CenterOffSet";
+        private const string S_TargetTexture = "S_TargetTexture";
         private const string S_Transform = "S_Transform";
         #endregion
 
@@ -17,13 +19,15 @@ namespace Altseed
         /// <param name="context">送信元の情報</param>
         protected RenderedCamera(SerializationInfo info, StreamingContext context) : base(new MemoryHandle(IntPtr.Zero))
         {
-            //var ptr = cbg_RenderedCamera_Create();
-            //if (ptr == IntPtr.Zero) throw new SerializationException("インスタンス生成に失敗しました");
+            var ptr = cbg_RenderedCamera_Create();
+            if (ptr == IntPtr.Zero) throw new SerializationException("インスタンス生成に失敗しました");
 
-            //selfPtr = ptr;
+            selfPtr = ptr;
 
-            //if (!cacheRepo.ContainsKey(ptr)) cacheRepo.Add(ptr, new WeakReference<RenderedCamera>(this));
+            if (!cacheRepo.ContainsKey(ptr)) cacheRepo.Add(ptr, new WeakReference<RenderedCamera>(this));
             Transform = info.GetValue<Matrix44F>(S_Transform);
+            CenterOffset = info.GetValue<Vector2F>(S_CenterOffSet);
+            TargetTexture = info.GetValue<RenderTexture>(S_TargetTexture);
         }
 
         /// <summary>
@@ -34,6 +38,8 @@ namespace Altseed
         protected virtual void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             if (info == null) throw new ArgumentNullException("引数がnullです", nameof(info));
+            info.AddValue(S_CenterOffSet, CenterOffset);
+            info.AddValue(S_TargetTexture, TargetTexture);
             info.AddValue(S_Transform, Transform);
         }
         void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context) => GetObjectData(info, context);
