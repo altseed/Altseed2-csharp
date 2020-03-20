cf703bf54de35c09671e8c27f91a5f3f2317346c
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-03-21 06:02:41 +0900

    fix helper

--- Engine/InternalHelper/CacheHelper.cs
+++ Engine/InternalHelper/CacheHelper.cs
@@ -41,20 +41,17 @@ namespace Altseed
             if (obj == null) throw new ArgumentNullException("引数がnullです", nameof(obj));
             if (native == IntPtr.Zero) throw new ArgumentNullException("引数がnullです", nameof(native));
 
-            var cacheRepo = (Dictionary<IntPtr, WeakReference<TClass>>)obj.CacheRepo;
-
-            if (cacheRepo.ContainsKey(native))
+            if (obj.CacheRepo.ContainsKey(native))
             {
-                cacheRepo[native].TryGetTarget(out var cacheSet);
+                obj.CacheRepo[native].TryGetTarget(out var cacheSet);
                 if (cacheSet != null)
                 {
                     cacheSet.Release();
                     return;
                 }
-                else cacheRepo.Remove(native);
+                else obj.CacheRepo.Remove(native);
             }
-
-            cacheRepo[native] = new WeakReference<TClass>(obj);
+            obj.CacheRepo[native] = new WeakReference<TClass>(obj);
 
             obj.Self = native;
         }
@@ -65,13 +62,24 @@ namespace Altseed
         /// <typeparam name="TClass">キャッシュ制御を行うクラス</typeparam>
         /// <param name="obj">キャッシュ制御を行うクラスのインスタンス</param>
         /// <param name="native"><paramref name="obj"/>に登録するポインタ</param>
+        /// <exception cref="ArgumentException">スレッドセーフなキャッシュ保存ディクショナリを取得できなかった</exception>
         /// <exception cref="ArgumentNullException"><paramref name="obj"/>または<paramref name="native"/>がnull</exception>
         internal static void CacheHandlingConcurrent<TClass>(TClass obj, IntPtr native) where TClass : class, IChacheKeeper<TClass>
         {
             if (obj == null) throw new ArgumentNullException("引数がnullです", nameof(obj));
             if (native == IntPtr.Zero) throw new ArgumentNullException("引数がnullです", nameof(native));
 
-            var cacheRepo = (ConcurrentDictionary<IntPtr, WeakReference<TClass>>)obj.CacheRepo;
+            ConcurrentDictionary<IntPtr, WeakReference<TClass>> cacheRepo;
+
+            try
+            {
+                cacheRepo = (ConcurrentDictionary<IntPtr, WeakReference<TClass>>)obj.CacheRepo;
+            }
+            catch (InvalidCastException)
+            {
+                throw new ArgumentException("スレッドセーフなキャッシュ保存ディクショナリを取得できませんでした");
+            }
+           
 
             if (cacheRepo.ContainsKey(native))
             {
