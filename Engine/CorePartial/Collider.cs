77bef27444326edf387aa1248f4c55f8d8bb0afe
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-04-08 03:00:41 +0900

    add collider serialization

--- Engine/CorePartial/Collider.cs
+++ Engine/CorePartial/Collider.cs
@@ -1,7 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Runtime.Serialization;
 
 namespace Altseed
 {
+    public partial class CircleCollider
+    {
+        partial void Deserialize_GetPtr(ref IntPtr ptr, SerializationInfo info) => ptr = cbg_CircleCollider_Constructor_0();
+    }
+    public partial class RectangleCollider
+    {
+        partial void Deserialize_GetPtr(ref IntPtr ptr, SerializationInfo info) => ptr = cbg_RectangleCollider_Constructor_0();
+    }
+    public partial class PolygonCollider
+    {
+        partial void Deserialize_GetPtr(ref IntPtr ptr, SerializationInfo info) => ptr = cbg_PolygonCollider_Constructor_0();
+    }
 }
