49b281611ce0e973485442f0a3bc871e8baaf611
wisteria <wisnkcs@gmail.com>
2020-04-08 00:30:55 +0900

    add Collider Test

---
+++ Test/Collider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Altseed.Test
+{
+    [TestFixture]
+    class Collider
+    {
+        [Test, Apartment(ApartmentState.STA)]
+        public void SpriteNode()
+        {
+            var tc = new TestCore();
+            tc.Duration = 2000;
+            tc.Init();
+
+            var texture = Texture2D.Load(@"../../Core/TestData/IO/AltseedPink.png");
+            Assert.NotNull(texture);
+
+            var node = new SpriteNode();
+            //node.Src = new RectF(new Vector2F(100, 100), new Vector2F(200, 200));
+            node.Texture = texture;
+            node.Position = new Vector2F(200, 200);
+            node.CenterPosition = texture.Size / 2;
+            node.Scale = new Vector2F(0.2f, 0.2f);
+            Engine.AddNode(node);
+
+            var col = new CircleCollider();
+            col.Radius = 200 * 0.2f;
+            col.Position = node.Position;
+
+            var node2 = new SpriteNode();
+            //node.Src = new RectF(new Vector2F(100, 100), new Vector2F(200, 200));
+            node2.Texture = texture;
+            node2.Position = new Vector2F(200, 200);
+            node2.CenterPosition = texture.Size / 2;
+            node2.Scale = new Vector2F(0.2f, 0.2f);
+            Engine.AddNode(node2);
+            var col2 = new CircleCollider();
+            col2.Radius = 200 * 0.2f;
+            col2.Position = node.Position;
+
+            tc.LoopBody(c =>
+            {
+                node2.Position = col2.Position = Engine.Mouse.Position;
+                if (col.GetIsCollidedWith(col2))
+                {
+                    node2.Angle++;
+                }
+            }
+            , null);
+
+            tc.End();
+        }
+    }
+}
