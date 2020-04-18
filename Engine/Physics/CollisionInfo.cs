359c57ab883f50eba1d4eb16a2f8e54a70e9006f
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-04-18 16:54:27 +0900

    add info & eventreceiver

---
+++ Engine/Physics/CollisionInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Altseed
+{
+    /// <summary>
+    /// 衝突判定に関する情報を格納したクラス
+    /// </summary>
+    [Serializable]
+    public sealed class CollisionInfo
+    {
+        /// <summary>
+        /// 自身のコライダを取得する
+        /// </summary>
+        public Collider SelfCollider { get; }
+
+        /// <summary>
+        /// 衝突相手のコライダを取得する
+        /// </summary>
+        public Collider TheirsCollider { get; }
+
+        internal CollisionInfo(Collider selfCollider, Collider theirsCollider)
+        {
+            SelfCollider = selfCollider ?? throw new ArgumentNullException(nameof(selfCollider), "引数がnullです");
+            TheirsCollider = theirsCollider ?? throw new ArgumentNullException(nameof(theirsCollider), "引数がnullです");
+        }
+    }
+}
---
+++ Engine/Physics/ICollisionEventReceiver.cs
@@ -0,0 +1,14 @@
+﻿namespace Altseed
+{
+    /// <summary>
+    /// 衝突判定を感知するイベントを提供するインターフェイス
+    /// </summary>
+    public interface ICollisionEventReceiver
+    {
+        /// <summary>
+        /// 衝突した時に呼び出される
+        /// </summary>
+        /// <param name="info">衝突に関する情報</param>
+        void OnCollision(CollisionInfo info);
+    }
+}
