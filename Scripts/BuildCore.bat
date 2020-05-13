5b6fad5c46d65ec1525c9979a8e67fe1d9dcbc01
GON <hutabasuzukiryu@live.jp>
2020-05-14 00:28:14 +0900

    update: submodule

--- Scripts/BuildCore.bat
+++ Scripts/BuildCore.bat
@@ -5,7 +5,7 @@ mkdir ..\Core\build
 
 cd /d ..\Core\build
 
-cmake -A x64 -D BUILD_TEST=OFF ../
+cmake -A x64 -D BUILD_TEST=OFF USE_MSVC_RUNTIME_LIBRARY_DLL=OFF ../
 
 cmake --build . --config Debug
 
