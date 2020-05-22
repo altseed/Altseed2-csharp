c88ed4b1e36d6b2a50e3ae89f124fda4d23f1377
GCLemon <green.c.lemon@gmail.com>
2020-05-22 13:49:01 +0900

    Remove shader code from C# source

--- Engine/Node/PostEffect/PostEffectLightBloomNode.cs
+++ Engine/Node/PostEffect/PostEffectLightBloomNode.cs
@@ -53,18 +53,20 @@ namespace Altseed
             _BlurYMaterial = new Material();
             _TextureMixer = new Material();
 
-            _DownSampler.SetShader(Shader.Create("DownSample", _DownSamplerCode, ShaderStageType.Pixel));
+            _DownSampler.SetShader(Shader.Create("DownSample", Engine.Graphics.BuiltinShader.DownsampleShader, ShaderStageType.Pixel));
 
-            var xBlurShader = Shader.Create("XBLur", "#define BLUR_X\n" + _BlurShaderCode, ShaderStageType.Pixel);
+            var blurShaderCode = Engine.Graphics.BuiltinShader.LightBloomShader;
+
+            var xBlurShader = Shader.Create("XBLur", "#define BLUR_X\n" + blurShaderCode, ShaderStageType.Pixel);
             _BlurXMaterial.SetShader(xBlurShader);
 
-            var xLumBlurShader = Shader.Create("XLumBLur", "#define BLUR_X\n#define LUM_MODE" + _BlurShaderCode, ShaderStageType.Pixel);
+            var xLumBlurShader = Shader.Create("XLumBLur", "#define BLUR_X\n#define LUM_MODE" + blurShaderCode, ShaderStageType.Pixel);
             _BlurXLumMaterial.SetShader(xLumBlurShader);
 
-            var yBlurShader = Shader.Create("YBLur", "#define BLUR_Y\n" + _BlurShaderCode, ShaderStageType.Pixel);
+            var yBlurShader = Shader.Create("YBLur", "#define BLUR_Y\n" + blurShaderCode, ShaderStageType.Pixel);
             _BlurYMaterial.SetShader(yBlurShader);
 
-            var mixerShader = Shader.Create("Mixer", _MixerShaderCode, ShaderStageType.Pixel);
+            var mixerShader = Shader.Create("Mixer", Engine.Graphics.BuiltinShader.TextureMixShader, ShaderStageType.Pixel);
             _TextureMixer.SetShader(mixerShader);
 
             Intensity = 5;
@@ -121,161 +123,5 @@ namespace Altseed
                 }
             }
         }
-
-        private readonly string _DownSamplerCode = @"
-
-Texture2D _MainTexture : register(t0);
-SamplerState _MainSampler : register(s0);
-
-struct PS_INPUT
-{
-    float4 Position : SV_POSITION;
-    float4 Color : COLOR0;
-    float2 UV1 : UV0;
-    float2 UV2 : UV1;
-};
-
-cbuffer Consts : register(b1)
-{
-    float4 _ImageSize;
-}
-
-float4 main(PS_INPUT input) : SV_TARGET 
-{
-	float4 color1 = _MainTexture.Sample(_MainSampler, input.UV1 + float2(-0.5, -0.5) / _ImageSize);
-	float4 color2 = _MainTexture.Sample(_MainSampler, input.UV1 + float2(+0.5, -0.5) / _ImageSize);
-	float4 color3 = _MainTexture.Sample(_MainSampler, input.UV1 + float2(-0.5, +0.5) / _ImageSize);
-	float4 color4 = _MainTexture.Sample(_MainSampler, input.UV1 + float2(+0.5, +0.5) / _ImageSize);
-	return (color1 + color2 + color3 + color4) * 0.25;
-}
-
-        ";
-
-
-
-        private readonly string _BlurShaderCode = @"
-
-struct PS_INPUT
-{
-    float4 Position : SV_POSITION;
-    float4 Color : COLOR0;
-    float2 UV1 : UV0;
-    float2 UV2 : UV1;
-};
-
-cbuffer Consts : register(b1)
-{
-    float4 _Resolution;
-    float4 _Intensity;
-    float4 _Threshold;
-    float4 _Exposure;
-};
-
-Texture2D _BaseTexture : register(t0);
-SamplerState _BaseSampler : register(s0);
-
-static float weight[4];
-
-float3 getLuminance(float3 color)
-{
-    return float3(color.x * 0.300000 + color.y * 0.590000 + color.z * 0.110000);
-}
-
-float gauss(float x, float sigma)
-{
-    return exp(- 0.5 * (x * x) / (sigma * sigma));
-}
-
-float3 getColor(float2 uv)
-{
-#ifdef BLUR_X
-    float4 color = _BaseTexture.Sample(_BaseSampler, uv) * _Exposure;
-    color.xyz = min(color.xyz, float4(255.0));
-
-#ifdef LUM_MODE
-    float3 lum = getLuminance(color.xyz);
-    float3 bloomedLum = lum - _Threshold;
-    float3 bloomedPower = min(max(bloomedLum / 2.0, 0.0), 1.0);
-    return color.xyz * bloomedPower;
-#else
-    float3 bloomedLum = color.xyz - _Threshold.xyz;
-    bloomedLum = max(bloomedLum, float3(0.0, 0.0, 0.0));
-    color.xyz = bloomedLum;
-    return color;
-#endif
-
-#endif
-
-#ifdef BLUR_Y
-    return _BaseTexture.Sample(_BaseSampler, uv);
-#endif
-}
-
-float4 getGaussianBlur(float2 uv)
-{
-    float weightTotal = 0;
-    for(int i = 0; i < 4; ++i)
-    {
-        weight[i] = gauss(i + 0.5, _Intensity.x);
-        weightTotal += weight[i] * 2.0;
-    }
-    
-    float3 outputColor = float3(0.0);
-
-    for(int i = 0; i < 4; ++i)
-    {
-#ifdef BLUR_X
-        float2 nShiftedUV = uv + float2(-(i + 0.5) / _Resolution.x, 0.0);
-        float2 pShiftedUV = uv + float2(+(i + 0.5) / _Resolution.x, 0.0);
-#endif
-#ifdef BLUR_Y
-        float2 nShiftedUV = uv + float2(0.0, -(i + 0.5) / _Resolution.y);
-        float2 pShiftedUV = uv + float2(0.0, +(i + 0.5) / _Resolution.y);
-#endif
-        outputColor += getColor(nShiftedUV) * weight[i] / weightTotal;
-        outputColor += getColor(pShiftedUV) * weight[i] / weightTotal;
-    }
-
-    return float4(outputColor, 1.0);
-}
-
-float4 main(PS_INPUT input) : SV_TARGET 
-{
-    return getGaussianBlur(input.UV1);
-}
-
-        ";
-
-
-
-        private readonly string _MixerShaderCode = @"
-
-struct PS_INPUT
-{
-    float4 Position : SV_POSITION;
-    float4 Color : COLOR0;
-    float2 UV1 : UV0;
-    float2 UV2 : UV1;
-};
-
-cbuffer Consts : register(b1)
-{
-    float4 _Weight;
-};
-
-Texture2D _Texture1 : register(t0);
-SamplerState _Sampler1 : register(s0);
-
-Texture2D _Texture2 : register(t1);
-SamplerState _Sampler2 : register(s1);
-
-float4 main(PS_INPUT input) : SV_TARGET 
-{
-    float4 color1 = _Texture1.Sample(_Sampler1, input.UV1);
-    float4 color2 = _Texture2.Sample(_Sampler2, input.UV1);
-    return color1 + color2 * _Weight;
-}
-
-        ";
     }
 }
