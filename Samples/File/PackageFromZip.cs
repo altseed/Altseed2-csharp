8dceb468bf4099e174691f2f475c643a64c3d32f
GCLemon <green.c.lemon@gmail.com>
2020-07-20 13:55:04 +0900

    Add file packaging sample

---
+++ Samples/File/PackageFromZip.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Altseed2;
+
+namespace Sample
+{
+    class PackageFromZip
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Altseed を初期化します。
+            if (!Engine.Initialize("SpriteNode", 640, 480)) return;
+
+            // Package.zip をルートパッケージにします。
+            Engine.File.AddRootPackage("Package.zip");
+
+            // パッケージに含まれる画像データをロードします。
+            var texture = Texture2D.Load(@"TestData/IO/AltseedPink256.png");
+
+            // 画像を描画するノードを生成・登録します。
+            var node = new SpriteNode();
+            node.Texture = texture;
+            Engine.AddNode(node);
+
+            // メインループ。
+            // Altseed のウインドウが閉じられると終了します。
+            while (Engine.DoEvents())
+            {
+                // Altseed を更新します。
+                Engine.Update();
+            }
+
+            // Altseed の終了処理をします。
+            Engine.Terminate();
+        }
+    }
+}
---
+++ Samples/File/PackageWithPassword.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Altseed2;
+
+namespace Sample
+{
+    class PackageWithPassword
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Altseed を初期化します。
+            if (!Engine.Initialize("SpriteNode", 640, 480)) return;
+
+            // TestData ディレクトリからファイルパッケージを生成します。
+            // パスワードを ALTSEED とします。
+            Engine.File.PackWithPassword("TestData", "Package.pack", "ALTSEED");
+
+            // Package.pack をルートパッケージにします。
+            Engine.File.AddRootPackageWithPassword("Package.pack", "ALTSEED");
+
+            // パッケージに含まれる画像データをロードします。
+            var texture = Texture2D.Load(@"TestData/IO/AltseedPink256.png");
+
+            // 画像を描画するノードを生成・登録します。
+            var node = new SpriteNode();
+            node.Texture = texture;
+            Engine.AddNode(node);
+
+            // メインループ。
+            // Altseed のウインドウが閉じられると終了します。
+            while (Engine.DoEvents())
+            {
+                // Altseed を更新します。
+                Engine.Update();
+            }
+
+            // Altseed の終了処理をします。
+            Engine.Terminate();
+        }
+    }
+}
