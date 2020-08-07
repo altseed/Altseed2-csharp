16a24f84712a3e3e7cdb9eb84fbf12d8d22aba5f
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-08-08 01:17:25 +0900

    temporary fixation of  RenderedCamera_Serilaization

--- Engine/CorePartial/Rendered.cs
+++ Engine/CorePartial/Rendered.cs
@@ -5,6 +5,16 @@ namespace Altseed2
 {
     internal partial class RenderedCamera
     {
+        partial void OnGetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue($"{S_RenderPassParameter}_Fix", RenderPassParameter);
+        }
+
+        partial void OnDeserialize_Constructor(SerializationInfo info, StreamingContext context)
+        {
+            RenderPassParameter = info.GetValue<RenderPassParameter>($"{S_RenderPassParameter}_Fix");
+        }
+
         partial void Deserialize_GetPtr(ref IntPtr ptr, SerializationInfo info)
         {
             ptr = cbg_RenderedCamera_Create();
