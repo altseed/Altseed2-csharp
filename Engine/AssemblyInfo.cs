aec23aa320ef84b61e73951f6f652014748ad1e9
wisteria <wisnkcs@gmail.com>
2020-08-14 21:52:32 +0900

    update version

--- Engine/AssemblyInfo.cs
+++ Engine/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-// [assembly: AssemblyTitle("Altseed")]
+[assembly: AssemblyTitle("Altseed2")]
 // [assembly: AssemblyDescription("")]
 // [assembly: AssemblyConfiguration("")]
 // [assembly: AssemblyCompany("")]
 // [assembly: AssemblyProduct("Altseed")]
-// [assembly: AssemblyCopyright("Copyright Â©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2020 Altseed")]
 // [assembly: AssemblyTrademark("")]
 // [assembly: AssemblyCulture("")]
 
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// [assembly: AssemblyVersion("1.0.0.0")]
-// [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
 [assembly: InternalsVisibleTo("Test")]
