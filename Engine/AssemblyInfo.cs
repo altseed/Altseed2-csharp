0f00e2eced0a6672517b02713a41d2fd2face3e4
wisteria <wisnkcs@gmail.com>
2020-08-14 22:05:25 +0900

    fix AssemblyInfo

--- Engine/AssemblyInfo.cs
+++ Engine/AssemblyInfo.cs
@@ -2,23 +2,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Altseed2")]
-// [assembly: AssemblyDescription("")]
-// [assembly: AssemblyConfiguration("")]
-// [assembly: AssemblyCompany("")]
-// [assembly: AssemblyProduct("Altseed")]
-[assembly: AssemblyCopyright("Copyright © 2020 Altseed")]
-// [assembly: AssemblyTrademark("")]
-// [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-// [assembly: ComVisible(false)]
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 // [assembly: Guid("2ecbef6c-727d-414d-a3b3-28db476b1fdf")]
 
@@ -29,7 +12,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
 
 [assembly: InternalsVisibleTo("Test")]
