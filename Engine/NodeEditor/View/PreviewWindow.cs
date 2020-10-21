e283e837226bb99136fae00cc80d7ea910e409ff
NumAniCloud <num.ani.cloud@gmail.com>
2020-10-22 02:47:40 +0900

    PreviewWindow:メソッドの抽出など

--- Engine/NodeEditor/View/PreviewWindow.cs
+++ Engine/NodeEditor/View/PreviewWindow.cs
@@ -3,6 +3,7 @@
     class PreviewWindow
     {
         private readonly IEditorPropertyAccessor _accessor;
+        private readonly NodeEditorPane _pane = new NodeEditorPane("Main");
 
         private Vector2I _windowSize = Engine.WindowSize;
         private RenderTexture _main;
@@ -11,8 +12,8 @@
         {
             _accessor = accessor;
 
-            _main = RenderTexture.Create(Engine.WindowSize - new Vector2I(600, 18), TextureFormat.R8G8B8A8_UNORM);
-            Engine._DefaultCamera.TargetTexture = _main;
+            UpdateRenderTexture(true,
+                RenderTexture.Create(Engine.WindowSize - new Vector2I(600, 18), TextureFormat.R8G8B8A8_UNORM));
         }
 
         public bool IsMainWindowFocus { get; private set; }
@@ -20,33 +21,45 @@
 
         public void UpdateMainWindow()
         {
-            if (_windowSize != Engine.WindowSize)
-            {
-                Vector2I texSize = Engine.WindowSize - new Vector2I(600, (int)_accessor.MenuHeight);
-                if (texSize.X > 0 && texSize.Y > 0)
-                    _main = RenderTexture.Create(texSize, TextureFormat.R8G8B8A8_UNORM);
-                Engine._DefaultCamera.TargetTexture = _main;
-                _windowSize = Engine.WindowSize;
-            }
+            AdjustWindowSize();
 
             var size = _windowSize - new Vector2F(600, _accessor.MenuHeight);
             var pos = new Vector2F(300, _accessor.MenuHeight);
-            Engine.Tool.SetNextWindowSize(size, ToolCond.None);
-            Engine.Tool.SetNextWindowPos(pos, ToolCond.None);
-            var flags = ToolWindowFlags.NoMove | ToolWindowFlags.NoBringToFrontOnFocus
-                | ToolWindowFlags.NoResize | ToolWindowFlags.NoScrollbar
-                | ToolWindowFlags.NoScrollbar | ToolWindowFlags.NoTitleBar | ToolWindowFlags.NoScrollWithMouse;
 
             Engine.Tool.PushStyleVarVector2F(ToolStyleVar.WindowPadding, default);
             Engine.Tool.PushStyleVarFloat(ToolStyleVar.WindowRounding, 0);
-            if (Engine.Tool.Begin("Main", flags))
+
+            _pane.Render(pos, size, () =>
             {
                 IsMainWindowFocus = Engine.Tool.IsWindowFocused(ToolFocused.None);
                 MousePosition = Engine.Mouse.Position - Engine.Tool.GetWindowPos();
                 Engine.Tool.Image(_main, _main.Size, default, new Vector2F(1, 1), new Color(255, 255, 255), new Color());
-                Engine.Tool.End();
-            }
+            });
+
             Engine.Tool.PopStyleVar(2);
+        }
+
+        private void AdjustWindowSize()
+        {
+            if (_windowSize != Engine.WindowSize)
+            {
+                Vector2I texSize = Engine.WindowSize - new Vector2I(600, (int) _accessor.MenuHeight);
+
+                UpdateRenderTexture(texSize.X > 0 && texSize.Y > 0,
+                    RenderTexture.Create(texSize, TextureFormat.R8G8B8A8_UNORM));
+
+                _windowSize = Engine.WindowSize;
+            }
+        }
+
+        private void UpdateRenderTexture(bool condition, RenderTexture renderTexture)
+        {
+            if (condition)
+            {
+                _main = renderTexture;
+            }
+
+            Engine._DefaultCamera.TargetTexture = _main;
         }
     }
 }
