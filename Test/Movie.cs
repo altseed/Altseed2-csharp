f6f08c0698f5f3def5e388c993f241ff9521c563
durswd <swda.durl@gmail.com>
2021-05-28 22:57:56 +0900

    Add test

---
+++ Test/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using NUnit.Framework;
+
+namespace Altseed2.Test
+{
+    [TestFixture]
+    public class Movie
+    {
+        [Test, Apartment(ApartmentState.STA)]
+        public void Basic()
+        {
+            var tc = new TestCore();
+            tc.Init();
+
+            var texture = Texture2D.Create(new Vector2I(640, 480));
+            Assert.NotNull(texture);
+
+            var node = new SpriteNode();
+            node.Texture = texture;
+            Engine.AddNode(node);
+
+            var mp = MediaPlayer.Load(@"TestData/Movie/Test1.mp4");
+            mp.Play(false);
+
+            tc.LoopBody(c =>
+            {
+                mp.WriteToTexture2D(texture);
+            }
+            , null);
+
+            tc.End();
+        }
+    }
+}
