5fd46d77a8069a3a0ebf2ee2e02f707b7cd5f433
uwe <40294263+UweSilver@users.noreply.github.com>
2021-07-13 11:30:33 +0900

    add TransformNodeManipulator

--- NodeEditor/GuiTool/TransformNodeManipulator/ParallelMove.cs
+++ NodeEditor/GuiTool/TransformNodeManipulator/ParallelMove.cs
@@ -3,30 +3,41 @@
 namespace Altseed2.NodeEditor
 {
     [Serializable]
-    internal class ParallelMove
+    internal class ParallelMove : TransformNodeManipulator
     {
-        private TransformNode _TransformNode;
         private RenderedPolygon _XArrow, _YArrow;
         private RenderedPolygon _CenterBox;
         private Vector2F[] _XArrowHitBox = new Vector2F[4];
         private Vector2F[] _YArrowHitBox = new Vector2F[4];
-        private PolygonCollider _Collider;
 
         private ControlState state = ControlState.none;
         private Vector2F _AdjustOffset = new Vector2F(0, 0);
 
         public Vector2F PositionInRenderedTexture { get { var vec = Engine._DefaultCamera.ProjectionMatrix.Transform3D(Engine._DefaultCamera.ViewMatrix.Transform3D(new Vector3F(_TransformNode.CenterPosition.X, _TransformNode.CenterPosition.Y, 0)));  return new Vector2F(vec.X, vec.Y); } }
 
-        
-
-        internal ParallelMove(TransformNode tranfromNode)
+        internal ParallelMove(TransformNode transformNode) : base (transformNode)
         {
-            _TransformNode = tranfromNode;
-            _Collider = new PolygonCollider();
-
             _XArrow = RenderedPolygon.Create();
             _YArrow = RenderedPolygon.Create();
             _CenterBox = RenderedPolygon.Create();
+        }
+
+        internal override void Update(Vector2F mousePosition)
+        {
+            Input(mousePosition);
+
+            UpdateArrow(mousePosition);
+            UpdateCenterBox();
+
+
+            
+            var position = GetScreenPosition(_TransformNode.Position);
+
+            if (Engine.Tool.BeginMainMenuBar())
+            {
+                Engine.Tool.Text("TrNd" + position);
+                Engine.Tool.EndMainMenuBar();
+            }
         }
 
         internal void Input(Vector2F mousePosition)
@@ -38,7 +49,7 @@ namespace Altseed2.NodeEditor
                 case ControlState.none:
                     if (IsInPreviewWindow(mousePosition))
                     {
-                        if(mouseLButton == ButtonState.Push)
+                        if (mouseLButton == ButtonState.Push)
                         {
                             _AdjustOffset = mousePosition - GetScreenPosition(_TransformNode.Position);
 
@@ -46,11 +57,11 @@ namespace Altseed2.NodeEditor
                             {
                                 state = ControlState.FreeDrag;
                             }
-                            else if(MouseHit(mousePosition, _XArrow.Vertexes))
+                            else if (MouseHit(mousePosition, _XArrow.Vertexes))
                             {
                                 state = ControlState.Xdrag;
                             }
-                            else if(MouseHit(mousePosition, _YArrow.Vertexes))
+                            else if (MouseHit(mousePosition, _YArrow.Vertexes))
                             {
                                 state = ControlState.Ydrag;
                             }
@@ -72,44 +83,6 @@ namespace Altseed2.NodeEditor
                     if (mouseLButton == ButtonState.Release) state = ControlState.none;
                     AdjustPosition(mousePosition - _AdjustOffset);
                     break;
-            }
-        }
-
-        private void AdjustPosition(Vector2F referenceScreenPosition)
-        {
-            const float error = 0.0001f;
-
-            if (Engine.Tool.BeginMainMenuBar())
-            {
-                Engine.Tool.Text("moving");
-                Engine.Tool.Text("offset =>" + _AdjustOffset.ToString());
-                
-                Engine.Tool.EndMainMenuBar();
-            }
-
-            var vec = (referenceScreenPosition - GetScreenPosition(_TransformNode.Position));
-
-            if (vec.Length > error)
-            {
-                _TransformNode.Position = _TransformNode.Position + vec * new Vector2F(1, -1) * 100f ;
-            }
-        }
-
-        internal void Update(Vector2F mousePosition)
-        {
-            Input(mousePosition);
-
-            UpdateArrow(mousePosition);
-            UpdateCenterBox();
-
-
-            
-            var position = GetScreenPosition(_TransformNode.Position);
-
-            if (Engine.Tool.BeginMainMenuBar())
-            {
-                Engine.Tool.Text("TrNd" + position);
-                Engine.Tool.EndMainMenuBar();
             }
         }
 
@@ -178,7 +151,7 @@ namespace Altseed2.NodeEditor
             renderedPolygon.SetDefaultIndexBuffer();
         }
 
-        internal void Draw()
+        internal override void Draw()
         {
             void draw(RenderedPolygon renderedPolygon)
             {
@@ -188,46 +161,6 @@ namespace Altseed2.NodeEditor
             draw(_XArrow);
             draw(_YArrow);
             draw(_CenterBox);
-        }
-
-        private bool MouseHit(Vector2F pos, VertexArray vertices)
-        {
-            Vector2F[] screenPosition = new Vector2F[vertices.Count];
-            for(int i = 0;i < screenPosition.Length;i++)
-            {
-                screenPosition[i] = GetScreenPosition(vertices[i].Position);
-            }
-
-
-
-            _Collider.Vertexes = screenPosition;
-
-            var mouseCollider = new CircleCollider();
-            mouseCollider.Radius = 0f;
-            mouseCollider.Position = pos;
-
-            return _Collider.GetIsCollidedWith(mouseCollider);
-        }
-
-        private Vector2F GetScreenPosition(Vector2F worldPosition)
-        {
-            var vector3 = Engine._DefaultCamera.ProjectionMatrix.Transform3D(Engine._DefaultCamera.ViewMatrix.Transform3D(new Vector3F(worldPosition.X, worldPosition.Y, 0)));
-            return new Vector2F(vector3.X, vector3.Y);
-        }
-
-        private Vector2F GetScreenPosition(Vector3F worldPosition)
-        {
-            var vector3 = Engine._DefaultCamera.ProjectionMatrix.Transform3D(Engine._DefaultCamera.ViewMatrix.Transform3D(new Vector3F(worldPosition.X, worldPosition.Y, worldPosition.Z)));
-            return new Vector2F(vector3.X, vector3.Y);
-        }
-
-        private bool IsInPreviewWindow(Vector2F position)
-        {
-            return
-                -1f <= position.X
-                && position.X <= 1f
-                && -1f <= position.Y
-                && position.Y <= 1f;
         }
 
         enum ControlState { none, Xdrag, Ydrag, FreeDrag}
--- NodeEditor/GuiTool/TransformNodeManipulator/TransformNodeManipulator.cs
+++ NodeEditor/GuiTool/TransformNodeManipulator/TransformNodeManipulator.cs
@@ -12,8 +12,59 @@ namespace Altseed2
             _TransformNode = transformNode;
         }
 
-        internal abstract void Update();
+        internal abstract void Update(Vector2F mousePosition);
 
         internal abstract void Draw();
+
+        internal protected bool MouseHit(Vector2F pos, VertexArray vertices)
+        {
+            Vector2F[] screenPosition = new Vector2F[vertices.Count];
+            for (int i = 0; i < screenPosition.Length; i++)
+            {
+                screenPosition[i] = GetScreenPosition(vertices[i].Position);
+            }
+
+            var collider = new PolygonCollider();
+            collider.Vertexes = screenPosition;
+
+            var mouseCollider = new CircleCollider();
+            mouseCollider.Radius = 0f;
+            mouseCollider.Position = pos;
+
+            return collider.GetIsCollidedWith(mouseCollider);
+        }
+
+        protected Vector2F GetScreenPosition(Vector2F worldPosition)
+        {
+            var vector3 = Engine._DefaultCamera.ProjectionMatrix.Transform3D(Engine._DefaultCamera.ViewMatrix.Transform3D(new Vector3F(worldPosition.X, worldPosition.Y, 0)));
+            return new Vector2F(vector3.X, vector3.Y);
+        }
+
+        internal protected void AdjustPosition(Vector2F referenceScreenPosition)
+        {
+            const float error = 0.0001f;
+
+            var vec = (referenceScreenPosition - GetScreenPosition(_TransformNode.Position));
+
+            if (vec.Length > error)
+            {
+                _TransformNode.Position = _TransformNode.Position + vec * new Vector2F(1, -1) * 100f;
+            }
+        }
+
+        protected Vector2F GetScreenPosition(Vector3F worldPosition)
+        {
+            var vector3 = Engine._DefaultCamera.ProjectionMatrix.Transform3D(Engine._DefaultCamera.ViewMatrix.Transform3D(worldPosition));
+            return new Vector2F(vector3.X, vector3.Y);
+        }
+
+        protected bool IsInPreviewWindow(Vector2F position)
+        {
+            return
+                -1f <= position.X
+                && position.X <= 1f
+                && -1f <= position.Y
+                && position.Y <= 1f;
+        }
     }
 }
